4440b60449d2321547052466630bc6cb141f5cd0
yunmin <362279869@qq.com>
2020-10-16 17:41:46 +0800

    test

--- example/game-server/app/servers/http/route/httpRoute.js
+++ example/game-server/app/servers/http/route/httpRoute.js
@@ -1,26 +1,16 @@
 'use strict';
 
 module.exports = function (app, http, plugin) {
-    if (plugin.useSSL) {
-        http.get('/testHttps', function (req, res, next) {
+    // plugin.useSSL: https/http
+    http.get('/', function (req, res, next) {
+        console.log('pid: %s', process.pid);
 
-            let custom_data = 'custom data';
-            res.set('resp', custom_data);
+        let custom_data = 'custom data';
+        res.set('resp', custom_data);
 
-            res.send('https success');
-            // Let the message flow down(to filer.after)
-            next();
-        });
-    } else {
-        http.get('/testHttp', function (req, res, next) {
-            console.log('pid: %s', process.pid);
+        res.send('http server work fine!');
 
-            let custom_data = 'custom data';
-            res.set('resp', custom_data);
-
-            res.send('http success');
-            // Let the message flow down(to filer.after)
-            next();
-        });
-    }
+        // Let the message flow down(to filer.after)
+        next();
+    });
 };
